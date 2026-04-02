e83a17cc563c74e0ef73a9ab4d41cee241038607
Miguel Solans <cmiguelsolans@gmail.com>
2026-04-02 18:18:21 +0100

    Convert project to SPM

---
+++ AppUIKit/Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AppUIKit",
+    platforms: [
+        .macOS(.v12), .iOS(.v16)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "AppUIKit",
+            targets: ["AppUIKit"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "AppUIKit",
+            path: "Sources"),
+        .testTarget(
+            name: "AppUIKitTests",
+            dependencies: ["AppUIKit"]),
+    ]
+)
